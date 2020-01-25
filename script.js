fd6014be4b07261b9e5d09a3498ce7fa2c2b9927
coletucker22 <59104432+coletucker22@users.noreply.github.com>
2020-01-25 13:37:17 -0600

    Recipe Instructions

---
+++ script.js
@@ -0,0 +1,12 @@
+//
+function marinaraSauce () {
+    console.log('Heat a medium-large sauce pan over medium heat')
+    console.log('Add 2 tspns of oil and 5 garlic cloves, cook until golden, about 2 minutes')
+    console.log('Add 1/4 cup water, 2 cans of crushed tomatoes, and salt and season with black pepper to taste')
+    console.log('Cover the pot, bring to a boil, reduce the heat to medium-low, and simmer until the sauce is heated, about 10 minutes')
+    console.log('Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed')
+
+
+}
+
+marinaraSauce()
